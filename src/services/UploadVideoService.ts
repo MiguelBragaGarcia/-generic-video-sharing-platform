7a88438cf3ddf59aea3cb1cde91a2c5d13faa389
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-07-23 21:32:45 -0300

    Criando service responsável pelo upload dos vídeos

---
+++ src/services/UploadVideoService.ts
@@ -0,0 +1,37 @@
+import Video from '../models/Video';
+import VideosRepository from '../repositories/VideosRepository';
+import DiskStorageProviderVideo from '../Providers/StorageProvider/implementations/DiskStorageProviderVideo';
+
+import AppError from '../errors/AppError';
+
+interface IRequest {
+  video_id: string;
+  user_id: string;
+  video_filename: string;
+}
+
+class UploadVideoService {
+  public async execute({
+    video_id,
+    user_id,
+    video_filename,
+  }: IRequest): Promise<Video> {
+    const videosRepository = new VideosRepository();
+    const storageProvider = new DiskStorageProviderVideo();
+
+    const video = await videosRepository.findById(video_id);
+
+    if (!video) {
+      throw new AppError('Missing video ID');
+    }
+
+    const filename = await storageProvider.saveFile(video_filename);
+
+    video.video = filename;
+
+    await videosRepository.save(video);
+
+    return video;
+  }
+}
+export default UploadVideoService;
