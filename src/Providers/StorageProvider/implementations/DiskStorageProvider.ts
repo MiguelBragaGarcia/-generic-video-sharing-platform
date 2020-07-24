373fc7b0afeae01d4f64c72bc0e7d026a8bb3f97
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-07-23 21:29:54 -0300

    Refatorando variáveis

--- src/Providers/StorageProvider/implementations/DiskStorageProvider.ts
+++ src/Providers/StorageProvider/implementations/DiskStorageProvider.ts
@@ -1,19 +1,19 @@
 import fs from 'fs';
 import path from 'path';
-import uploadAvatarConfig from '../../../config/uploadAvatarConfig';
+import uploadConfig from '../../../config/upload';
 import IStorageProvider from '../models/IStorageProvider';
 
 class DiskStorageProvider implements IStorageProvider {
   public async saveFile(file: string): Promise<string> {
     await fs.promises.rename(
-      path.resolve(uploadAvatarConfig.tmpFolder, file),
-      path.resolve(uploadAvatarConfig.uploadFolder, file)
+      path.resolve(uploadConfig.tmpFolder, file),
+      path.resolve(uploadConfig.uploadAvatarFolder, file)
     );
     return file;
   }
 
   public async deleteFile(file: string): Promise<void> {
-    const filePath = path.resolve(uploadAvatarConfig.uploadFolder, file);
+    const filePath = path.resolve(uploadConfig.uploadAvatarFolder, file);
 
     try {
       await fs.promises.stat(filePath);
