04890fe06a7a7d3dc5c35a4a99f49d50fed7b7c1
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-07-25 13:43:36 -0300

    Corrigindo validação de dados na rota

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -15,7 +15,8 @@ profileRouter.put(
     [Segments.BODY]: {
       name: Joi.string().required(),
       email: Joi.string().email().required(),
-      password: Joi.string().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
       password_confirmation: Joi.string().valid(Joi.ref('password')),
     },
   }),
