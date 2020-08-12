07277fa2724ef94b12b58d10065d9cf26443c0f2
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-08-12 18:53:04 -0300

    Removendo campo password da resposta da requisição

--- src/modules/videos/infra/controllers/SearchController.ts
+++ src/modules/videos/infra/controllers/SearchController.ts
@@ -2,15 +2,16 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import FindVideoService from '@modules/videos/services/FindVideoService';
+import { classToClass } from 'class-transformer';
 
 class SearchController {
-  public async show(request: Request, response: Response): Promise<Response> {
+  public async create(request: Request, response: Response): Promise<Response> {
     const { searchTags } = request.body;
     const findVideoService = container.resolve(FindVideoService);
 
     const videos = await findVideoService.execute(searchTags);
 
-    return response.json(videos);
+    return response.json(classToClass(videos));
   }
 }
 
