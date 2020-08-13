ff9a9a27a29cc4c5a29f09eb98ee3842d2aef021
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-08-13 16:35:22 -0300

    Adicionando os tipos das novas funcionalidades

--- src/modules/videos/repositories/ITagsRepository.ts
+++ src/modules/videos/repositories/ITagsRepository.ts
@@ -4,4 +4,6 @@ import Tag from '../infra/typeorm/schemas/Tag';
 export default interface ITagsRepository {
   create(data: ICreateIndexTagDTO): Promise<void>;
   findByTags(tags: string[]): Promise<Tag[] | undefined>;
+  save(data: Tag): Promise<void>;
+  findByVideoId(video_id: string): Promise<Tag | undefined>;
 }
