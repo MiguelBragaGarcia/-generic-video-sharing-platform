d88c77c55c74ac3280965e5f743fe56bac1340ec
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-08-13 16:33:13 -0300

    Refatorando legibilidade por causa de interface

--- src/modules/videos/repositories/IVideosRepository.ts
+++ src/modules/videos/repositories/IVideosRepository.ts
@@ -1,11 +1,11 @@
 import Video from '@modules/videos/infra/typeorm/entities/Video';
 import User from '@modules/users/infra/typeorm/entities/User';
-import IFindVideosDTO from '../dtos/IFindVideosDTO';
+import IPaginateOptionsDTO from '../dtos/IPaginateOptionsDTO';
 
 export default interface IVideosRepository {
   findById(id: string): Promise<Video | undefined>;
   create(user: User): Promise<Video>;
   save(video: Video): Promise<Video>;
   delete(video_id: string): Promise<void>;
-  find(data: IFindVideosDTO): Promise<Video[] | undefined>;
+  find(data: IPaginateOptionsDTO): Promise<Video[] | undefined>;
 }
