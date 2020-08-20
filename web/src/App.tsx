2bc56ddcd22fb2c02dd284f5266e51ce35aadc90
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-08-20 20:40:52 -0300

    Trocando componente estático pelo roteador do react router dom

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import Dashboard from './pages/Dashboard';
+import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <Dashboard />
+    <Router>
+      <Routes />
+    </Router>
   </>
 );
 
