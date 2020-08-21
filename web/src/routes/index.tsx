d8b91a36d4e9c511617bff15cf15a24fa700eb8f
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-08-21 20:25:19 -0300

    Adicionando novas rotas

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 
 import Dashboard from '../pages/Dashboard';
-import Login from '../pages/Login';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/login" component={Login} />
+    <Route path="/signin" component={SignIn} />
+    <Route path="/signup" component={SignUp} />
+    <Route path="/forgot" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
+
+    <PrivateRoute path="/profile" component={Profile} />
   </Switch>
 );
 
