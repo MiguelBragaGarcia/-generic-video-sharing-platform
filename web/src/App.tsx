36abe1d0d1bb7556e8e580b8e797e7836e27ba93
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-08-21 20:24:01 -0300

    Configurando a aplicação para funcionar com o hook de autenticação global

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
     <Router>
-      <Routes />
+      <AppProvider>
+        <Routes />
+      </AppProvider>
     </Router>
   </>
 );
