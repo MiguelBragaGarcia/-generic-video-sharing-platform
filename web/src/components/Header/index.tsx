529e12a21a536ebe605e8b9666c1b55243ff1c8d
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-08-26 19:37:11 -0300

    Criando componente Header

---
+++ web/src/components/Header/index.tsx
@@ -0,0 +1,85 @@
+import React, { useState, useCallback } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import { GoSearch } from 'react-icons/go';
+import { FiLogIn } from 'react-icons/fi';
+import { MdVideoCall } from 'react-icons/md';
+import { FaUserCircle } from 'react-icons/fa';
+
+import {
+  Container,
+  HeaderContainer,
+  MainPageButton,
+  ButtonActionsContainer,
+  SearchBar,
+  LoginButton,
+  SendVideoButton,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/Auth';
+
+const Header: React.FC = () => {
+  const history = useHistory();
+
+  const [searchInput, setSearchInput] = useState('');
+  const { user } = useAuth();
+
+  const handleSearch = useCallback(() => {
+    const parsedString = searchInput.replace(' ', '%');
+
+    history.push({
+      pathname: '/results',
+      search: `?search=${parsedString}`,
+    });
+  }, [history, searchInput]);
+
+  return (
+    <Container>
+      <HeaderContainer>
+        <Link to="/">
+          <MainPageButton>
+            <img src={logoImg} alt="logo" />
+          </MainPageButton>
+        </Link>
+
+        <SearchBar>
+          <input
+            type="text"
+            placeholder="Buscar por vídeos"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <button type="button" onClick={handleSearch}>
+            <GoSearch size={24} color="#FFF" />
+          </button>
+        </SearchBar>
+
+        <ButtonActionsContainer>
+          <SendVideoButton>
+            <MdVideoCall size={30} />
+          </SendVideoButton>
+
+          {user ? (
+            <Link to="/profile">
+              {user.avatar_url ? (
+                <img src={user.avatar_url} alt={user.name} />
+              ) : (
+                <FaUserCircle />
+              )}
+            </Link>
+          ) : (
+            <Link to="/signin">
+              <LoginButton>
+                <FiLogIn size={24} />
+                Fazer Login
+              </LoginButton>
+            </Link>
+          )}
+        </ButtonActionsContainer>
+      </HeaderContainer>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ web/src/components/Header/styles.ts
@@ -0,0 +1,121 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  height: 6rem;
+  width: 100vw;
+  background: #fff;
+  display: flex;
+`;
+
+export const HeaderContainer = styled.div`
+  width: 90vw;
+  display: flex;
+  margin: 0 auto;
+
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export const MainPageButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-style: none;
+  background: none;
+  margin-right: 2rem;
+
+  img {
+    height: 4rem;
+    width: 4rem;
+  }
+`;
+
+export const SearchBar = styled.div`
+  display: flex;
+
+  input {
+    width: 17rem;
+    height: 4.5rem;
+    border-radius: 1rem 0 0 1rem;
+    padding: 0.2rem 1rem;
+    border-style: none;
+    background: #eee;
+    color: #38393d;
+  }
+
+  button {
+    height: 4.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.4rem 1rem;
+
+    background: #3052d9;
+    border-style: none;
+    border-radius: 0 1rem 1rem 0;
+  }
+
+  @media (min-width: 700px) {
+    input {
+      width: 40rem;
+    }
+  }
+`;
+
+export const ButtonActionsContainer = styled.div`
+  display: flex;
+
+  align-items: center;
+
+  > a {
+    img {
+      width: 3.5rem;
+      height: 3.5rem;
+      border-radius: 50%;
+
+      svg {
+        width: 3.5rem;
+        height: 3.5rem;
+      }
+    }
+  }
+`;
+
+export const SendVideoButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin: 0 1rem;
+  background: none;
+  border-style: none;
+
+  @media (min-width: 700px) {
+    margin-right: 3rem;
+  }
+`;
+
+export const LoginButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 4.5rem;
+  background: transparent;
+  border-radius: 1rem;
+  border-style: none;
+  border: 1px solid #3052d9;
+
+  padding: 0.5rem;
+
+  color: #3052d9;
+
+  svg {
+    margin-right: 0.5rem;
+  }
+`;
