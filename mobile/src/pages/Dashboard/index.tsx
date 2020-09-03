3c04898851ededc303b2b4210117f0057c684313
Miguel Braga Garcia <miguelbragagarcia@gmail.com>
2020-09-03 20:38:43 -0300

    Trabalhando na página de Dashboard

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { TouchableHighlight } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import logoImg from '../../assets/logo.png';
+
 import {
+  Header,
+  NormalHeader,
+  SearchHeader,
+  LogoImage,
+  SearchInput,
   VideosList,
   Video,
   VideoThumbnail,
@@ -9,115 +21,175 @@ import {
   Views,
 } from './styles';
 
-const Dashboard: React.FC = () => (
-  <>
-    <VideosList>
-      <Video>
-        <VideoThumbnail
-          source={{
-            uri:
-              'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
-          }}
-        />
+const Dashboard: React.FC = () => {
+  const navigation = useNavigation();
 
-        <InfoContainer>
-          <AvatarImage
+  const [showSearchBar, setShowSearchBar] = useState(false);
+  const [search, setSearch] = useState('');
+
+  const toggleSearchBar = useCallback(() => {
+    setShowSearchBar(state => !state);
+  }, []);
+
+  const handleSearch = useCallback(() => {
+    navigation.navigate('Search', { search });
+  }, [navigation, search]);
+  return (
+    <>
+      <Header>
+        {showSearchBar ? (
+          <SearchHeader>
+            <TouchableHighlight onPress={toggleSearchBar}>
+              <Icon name="chevron-left" size={30} color="#333" />
+            </TouchableHighlight>
+            <SearchInput
+              autoFocus
+              onChangeText={text => setSearch(text)}
+              value={search}
+              placeholder="Busque por vídeos"
+              returnKeyType="send"
+              onSubmitEditing={handleSearch}
+            />
+          </SearchHeader>
+        ) : (
+          <NormalHeader>
+            <LogoImage source={logoImg} />
+            <TouchableHighlight onPress={toggleSearchBar}>
+              <Icon name="search" size={30} color="#333" />
+            </TouchableHighlight>
+          </NormalHeader>
+        )}
+      </Header>
+
+      <VideosList>
+        <Video>
+          <VideoThumbnail
             source={{
               uri:
                 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
             }}
           />
 
-          <Title>Título do vídeo</Title>
-          <Views>10000 vizualizações</Views>
-        </InfoContainer>
-      </Video>
+          <InfoContainer>
+            <AvatarImage
+              source={{
+                uri:
+                  'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+              }}
+            />
 
-      <Video>
-        <VideoThumbnail
-          source={{
-            uri:
-              'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
-          }}
-        />
+            <Title numberOfLines={1}>Título do vídeo muito longo </Title>
+            <Views numberOfLines={1}>10000000 vizualizações</Views>
+          </InfoContainer>
+        </Video>
 
-        <InfoContainer>
-          <AvatarImage
+        <Video>
+          <VideoThumbnail
             source={{
               uri:
                 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
             }}
           />
 
-          <Title>Título do vídeo</Title>
-          <Views>10000 vizualizações</Views>
-        </InfoContainer>
-      </Video>
+          <InfoContainer>
+            <AvatarImage
+              source={{
+                uri:
+                  'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+              }}
+            />
 
-      <Video>
-        <VideoThumbnail
-          source={{
-            uri:
-              'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
-          }}
-        />
+            <Title numberOfLines={1}>Título do vídeo muito longo </Title>
+            <Views numberOfLines={1}>10000000 vizualizações</Views>
+          </InfoContainer>
+        </Video>
 
-        <InfoContainer>
-          <AvatarImage
+        <Video>
+          <VideoThumbnail
             source={{
               uri:
                 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
             }}
           />
 
-          <Title>Título do vídeo</Title>
-          <Views>10000 vizualizações</Views>
-        </InfoContainer>
-      </Video>
+          <InfoContainer>
+            <AvatarImage
+              source={{
+                uri:
+                  'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+              }}
+            />
 
-      <Video>
-        <VideoThumbnail
-          source={{
-            uri:
-              'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
-          }}
-        />
+            <Title numberOfLines={1}>Título do vídeo muito longo </Title>
+            <Views numberOfLines={1}>10000000 vizualizações</Views>
+          </InfoContainer>
+        </Video>
 
-        <InfoContainer>
-          <AvatarImage
+        <Video>
+          <VideoThumbnail
             source={{
               uri:
                 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
             }}
           />
 
-          <Title>Título do vídeo</Title>
-          <Views>10000 vizualizações</Views>
-        </InfoContainer>
-      </Video>
+          <InfoContainer>
+            <AvatarImage
+              source={{
+                uri:
+                  'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+              }}
+            />
 
-      <Video>
-        <VideoThumbnail
-          source={{
-            uri:
-              'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
-          }}
-        />
+            <Title numberOfLines={1}>Título do vídeo muito longo </Title>
+            <Views numberOfLines={1}>10000000 vizualizações</Views>
+          </InfoContainer>
+        </Video>
 
-        <InfoContainer>
-          <AvatarImage
+        <Video>
+          <VideoThumbnail
             source={{
               uri:
                 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
             }}
           />
 
-          <Title>Título do vídeo</Title>
-          <Views>10000 vizualizações</Views>
-        </InfoContainer>
-      </Video>
-    </VideosList>
-  </>
-);
+          <InfoContainer>
+            <AvatarImage
+              source={{
+                uri:
+                  'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+              }}
+            />
+
+            <Title numberOfLines={1}>Título do vídeo muito longo </Title>
+            <Views numberOfLines={1}>10000000 vizualizações</Views>
+          </InfoContainer>
+        </Video>
+
+        <Video>
+          <VideoThumbnail
+            source={{
+              uri:
+                'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+            }}
+          />
+
+          <InfoContainer>
+            <AvatarImage
+              source={{
+                uri:
+                  'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+              }}
+            />
+
+            <Title numberOfLines={1}>Título do vídeo muito longo </Title>
+            <Views numberOfLines={1}>10000000 vizualizações</Views>
+          </InfoContainer>
+        </Video>
+      </VideosList>
+    </>
+  );
+};
 
 export default Dashboard;
--- mobile/src/pages/Dashboard/styles.ts
+++ mobile/src/pages/Dashboard/styles.ts
@@ -1,10 +1,48 @@
 import styled from 'styled-components/native';
+import { darken } from 'polished';
+
+import { TextInput } from 'react-native';
+
+export const Header = styled.View`
+  height: 60px;
+  justify-content: center;
+  border-bottom-width: 1px;
+  border-bottom-color: #c5c5c5;
+  padding: 0 10px;
+`;
+
+export const NormalHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const SearchHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const LogoImage = styled.Image`
+  width: 35px;
+  height: 35px;
+`;
+
+export const SearchInput = styled(TextInput)`
+  padding: 5px 16px;
+  font-size: 16px;
+  color: #000;
+  width: 90%;
+  background: #eee;
+  border-radius: 20px;
+`;
 
 export const VideosList = styled.ScrollView``;
 
 export const Video = styled.View`
-  margin-top: 10px;
-  /* colocar borda de 90% com color #eee */
+  border-bottom-width: 1px;
+  border-bottom-color: ${darken(0.05, '#eee')};
+
+  margin-bottom: 10px;
 `;
 
 export const VideoThumbnail = styled.Image`
@@ -23,7 +61,12 @@ export const AvatarImage = styled.Image`
   border-radius: 20px;
 `;
 export const Title = styled.Text`
+  color: #333;
   font-weight: bold;
   font-size: 18px;
+  max-width: 50%;
 `;
-export const Views = styled.Text``;
+export const Views = styled.Text`
+  width: 35%;
+  color: #333;
+`;
